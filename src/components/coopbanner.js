80e1b6de60892117cbf4ea5de38d6ae0d87818c8
Jake Bisson <jbiss085@uottawa.ca>
2021-01-11 09:43:52 -0500

    Made the coop banner title color lighter

--- src/components/coopbanner.js
+++ src/components/coopbanner.js
@@ -4,7 +4,7 @@ export default function coopbanner() {
     return (
         <div className="bg-secondary">
             <div className="py-6 px-16 md:px-40 lg:px-48 xl:px-64">
-                <p className="text-primary text-4xl font-title font-bold">I'm searching for a co-op placement...</p>
+                <p className="text-primary-lighter text-4xl font-title font-bold">I'm searching for a co-op placement...</p>
                 <div className="text-silver text-xl font-title font-bold">
                     <p>(Summer 2021, May-August)</p>
                     <p>Full stack, Front-end, or Back-end Developer</p>
