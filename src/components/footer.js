d36be97c778536617de9998ae76646f1af95add5
Jake Bisson <jbiss085@uottawa.ca>
2021-05-16 13:56:28 -0400

    removed coop banner from footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,8 +5,8 @@ import SocialMediaButtons from "./socialMediaButtons";
 export default function Footer() {
     return (
         <footer>
-            <CoopBanner/>
-            <div className="className=bg-silver px-16 md:px-24 lg:px-48 xl:px-64 py-6 text-primary">
+            {/* <CoopBanner/> */}
+            <div className="bg-silver px-16 md:px-24 lg:px-48 xl:px-64 py-6 text-primary">
                 <div class="flex flex-col sm:flex-row justify-between items-center flex-wrap">
                     <div>
                         <a href="https://drive.google.com/file/d/1sTZUdsdrZuk-FGD794YvGeAi-Jame3Ys/view?usp=sharing" target="_blank" className="rounded-xl text-silver bg-primary font-body p-3 hover:shadow-lg duration-200">Resume</a>
